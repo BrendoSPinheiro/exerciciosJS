f4b9dd6ba0c54d95a203f560e76b9a7136f33cef
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-01-09 13:53:37 -0300

    feat: update questao6

--- questoes/Questao06.js
+++ questoes/Questao06.js
@@ -1,7 +1,7 @@
 /**
  * Escreva uma função que receba um valor booleano ou numérico. Se o parâmetro fornecido for booleano, o
  * retorno da função deverá ser o inverso. Por exemplo, se a entrada for false, retornará true. Se o parâmetro for
- * numérico, o retorno será o número inverso. Por exemplo, se for fornecido 1, o retorno será 1. Se o parâmetro
+ * numérico, o retorno será o número inverso. Por exemplo, se for fornecido 1, o retorno será -1. Se o parâmetro
  * de entrada não for de nenhum dos tipo acima, retorne "booleano ou número esperados, mas o parâmetro é do
  * tipo ...".
  * 
@@ -22,4 +22,6 @@ function inverso(valor) {
   }
 }
 
-console.log(inverso(-2000));
+const valorInverso = inverso(-2000);
+
+console.log(valorInverso);
