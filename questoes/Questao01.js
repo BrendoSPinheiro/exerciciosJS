9be8a249365942eabacb9499f043f350cd7f74c3
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-01-08 21:59:54 -0300

    feat: primeira questao

---
+++ questoes/Questao01.js
@@ -0,0 +1,14 @@
+/**
+ * Crie uma função que retorna a string "Olá, " concatenada com um argumento text (a ser passado para a
+ * função) e com ponto de exclamação "!" no final.
+ * 
+ * exemplos:
+ * cumprimentar("Leonardo") // retornará "Olá, Leonardo!"
+ * cumprimentar("Maria") // retornará "Olá, Maria!"
+ */
+
+function imprimirOla(text) {
+  return `Olá, ${text}!`;
+}
+
+console.log(imprimirOla('Brendo'));
